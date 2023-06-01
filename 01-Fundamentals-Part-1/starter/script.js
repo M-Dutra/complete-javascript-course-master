dc5580e22b457564ddf02b10ff20b08c0c7882ce
Manuel Dutra <manuel.f.dutra8@gmail.com>
2023-06-01 12:24:50 +0200

    Part 1 Fundamentals - Completed

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,117 @@
-let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+// //Store variables
+// let firstName = "Jonas";
+// console.log(firstName);
+// camelCase = first, firstName, firstNamePerson
+// pode conter _ ou $ no no e das variaveis
+// let jonas_matilda = "JM";
+// let $function = 27;
+
+// let myFirstJob = "Civil Engineer";
+
+// let myCurrentJob = "Programmer";
+
+// console.log(myCurrentJob);
+
+
+// Logical operators
+// AND - OR - NOT
+
+// const hasDriveLicense = true;
+// const hasGoodVision = true;
+
+// console.log(hasDriveLicense && hasGoodVision); // AND
+// console.log(hasDriveLicense ||  hasGoodVision); // OR
+// console.log(!hasDriveLicense); // NOT
+
+// const shouldDrive = hasDriveLicense && hasGoodVision;
+
+// if (shouldDrive) {
+//   console.log('Sarah is able to drive!');
+// } else {
+//   console.log('Someone else should drive...');
+// }
+
+// const isTired = false;
+
+// if (hasDriveLicense && hasGoodVision && !isTired) {
+//   console.log('Sarah is able to drive!');
+// } else {
+//   console.log('Someone else should drive...');
+// }
+
+// Coding Challenge #3
+// const scoreDolphins = (96+108+89)/3;
+// const scoreKoalas = (88+91+110)/3;
+
+// if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+//   console.log('Dolphins win the competition');
+// } else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+//   console.log('Koalas win the competition');
+// } else if (scoreDolphins === scoreKoalas && scoreKoalas >= 100 && scoreDolphins >=100) {
+//   console.log ('Both wins the competition');
+// } else {
+//   console.log('No one wins...it is a draw')
+// }
+
+
+// Instead of if else - lets use switch
+
+// const day = 'monday';
+
+// switch(day) {
+
+//   case 'monday':
+//     console.log('Plan my course structure');
+//     console.log('Go to coding meetup');
+//     break;
+
+//   case 'tuesdaday':
+//     console.log('Prepare theory videos');
+//     break;
+
+
+//   case 'wednesday':
+//   case 'thursday':
+//     console.log('Write code examples');
+//     break;
+
+
+//   case 'friday':
+//     console.log('Record videos');
+//     break;
+
+
+//   case 'saturday':
+//   case 'sunday':
+//     console.log('Enjoy the weekend');
+//     break;🍷
+
+//   default:
+//     console.log('Not a valid day');
+// }
+
+
+// Statement and expressions
+
+// The condional operator - Ternary
+// const age = 23;
+// age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I dont have enought age to this 💧')
+// const drink = age >= 18 ? "🍷" : "💧";
+// console.log(drink);
+// console.log(`I like to drink ${age >= 18 ? 'wine 🍷 ' : 'water💧'}`);
+
+
+// Coding Challange #4
+// const bill = 275
+// const tip = (bill >= 50 && bill <= 300 ) ? bill * 0.15 : bill * 0.20;
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+// ES5 - ES6 - ESNext
+// ES6 - year 2015 - biggest update to the language ever
+// Babel - to production - transpile and polyfill your code (convert back to ES5) - compatibility dor all users
+
