ad05232ed7e8bb82f2d1018c1cbaaee54bbb48a2
Manuel Dutra <manuel.f.dutra8@gmail.com>
2023-06-02 17:40:16 +0200

    Part 5 - Developer Skills & Editor Setup

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -486,29 +486,29 @@
 
                               // Coding Challange #4
 
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
 
-const calcTip = function (bill) {
-  return bill >= 50 && bill < 300 ? bill * 0.15 : bill * 0.20;
-}
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill < 300 ? bill * 0.15 : bill * 0.20;
+// }
 
-for (let i = 0; i <= bills.length-1; i++){
-  const tip = calcTip(bills[i]);
-  tips.push(tip);
-  totals.push(tip + bills[i]);
-}
+// for (let i = 0; i <= bills.length-1; i++){
+//   const tip = calcTip(bills[i]);
+//   tips.push(tip);
+//   totals.push(tip + bills[i]);
+// }
 
-console.log(bills);
-console.log(tips);
-console.log(totals);
+// console.log(bills);
+// console.log(tips);
+// console.log(totals);
 
-const calcAverage = function(arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
-  }
-  return sum / arr.length;
-}
-console.log(calcAverage(totals));
+// const calcAverage = function(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+//   }
+//   return sum / arr.length;
+// }
+// console.log(calcAverage(totals));
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,60 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+                                                          // Problem 1 - Amplitude
+
+
+// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// const calcAmplitude = function (temps) {
+
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for(let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+
+//   console.log(max, min);
+
+//   const amplitude =  max - min;
+//   return amplitude;
+// }
+// console.log(calcAmplitude(temperatures));
+
+
+
+                                                  // Problem 2 - Receive 2 arrays
+
+
+
+// console.log(`-------Problem 2--------`);
+
+// const temperatures2 = [2000];
+
+// const calcAmplitudeNew = function (t1, t2) {
+
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for(let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+
+//   console.log(max, min);
+
+//   const amplitude =  max - min;
+//   return amplitude;
+// }
+// console.log(calcAmplitudeNew(temperatures, temperatures2));
