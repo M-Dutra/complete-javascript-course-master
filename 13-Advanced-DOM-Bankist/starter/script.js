03cbc3f7291ed9f900e08dc84620890edb059a02
Manuel Dutra <manuel.f.dutra8@gmail.com>
2023-06-27 18:55:05 +0200

    Video 200

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -11,6 +11,8 @@ const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 
 const openModal = function (e) {
@@ -166,9 +168,159 @@ const handleHover = function(e) {
 
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout',handleHover.bind(1));
+// ....................................................................
+
 
 
 // ....................................................................
+// Sticky Navigation
+
+// 1 Solution: bad performance: when we scroll all the times triggers the console.log(window.scrollY);
+// const initialCoords = section1.getBoundingClientRect();
+// // console.log(initialCoords);
+
+// window.addEventListener('scroll', function (e) {
+//   console.log(window.scrollY);
+//   if (window.scrollY > initialCoords.top) {
+//     nav.classList.add('sticky')
+//   } else {
+//     nav.classList.remove('sticky');
+//   }
+// });
+
+
+// 2 Solution - The intersection Observer API
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOptions = {
+//   root: null,
+//   treshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+//...
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  treshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+// ....................................................................
+
+
+
+
+// ....................................................................
+// Revealing elements on Scroll
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+
+allSections.forEach(function(section) {
+  sectionObserver.observe(section);
+  // section.classList.add('.section--hidden');
+});
+// ....................................................................
+
+
+// ....................................................................
+// Lazy Loading Images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+// ....................................................................
+
+
+// ....................................................................
+//Slider Component
+// Images exercice
+
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const slider = document.querySelector('.slider');
+slider.style.transform = 'scale(0.2) transleX(-800px)';
+slider.style.overflow = 'visible';
+
+const goToSlide = function(slide) {
+  slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`));
+};
+
+goToSlide(0);
+
+// Next Slide
+
+const nextSlide = function() {
+  if (curSlide === maxSlide -1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+}
+
+
+btnRight.addEventListener('click', nextSlide);
+
+
+
+
+// ....................................................................
+
+
+
 
 
 
@@ -311,8 +463,7 @@ nav.addEventListener('mouseout',handleHover.bind(1));
 
 // ....................................................................
 //  Smooth Scrolling
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+
 
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect(); // need to give the coordinates where to scroll
@@ -468,4 +619,28 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000); // apos 3
 // ....................................................................
 // Passing Arguments to Event Handlers - line 107
 
+// ....................................................................
+
+
+// ....................................................................
+// Sticky Navigation - line 174
+// ....................................................................
+
+
+// ....................................................................
+// Sticky Navigation - The intersection observer API - line 192
+// ....................................................................
+
+
+// ....................................................................
+// Revealing elements on Scroll
+// ....................................................................
+
+// ....................................................................
+// Lazy Loading Images - line 258
+// ....................................................................
+
+
+// ....................................................................
+// Slider - line 285
 // ....................................................................
